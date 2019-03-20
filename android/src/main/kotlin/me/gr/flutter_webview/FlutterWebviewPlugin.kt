f66625b94313cea68718da6f036284e444bfb2bf
cv0cv0 <linux2.0@foxmail.com>
2019-03-20 15:27:47 +0800

    first commit

--- android/src/main/kotlin/me/gr/flutter_webview/FlutterWebviewPlugin.kt
+++ android/src/main/kotlin/me/gr/flutter_webview/FlutterWebviewPlugin.kt
@@ -1,25 +1,28 @@
 package me.gr.flutter_webview
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterWebviewPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_webview")
-      channel.setMethodCallHandler(FlutterWebviewPlugin())
-    }
-  }
+class FlutterWebviewPlugin(private val activity: Activity) : MethodCallHandler {
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "flutter_webview")
+            channel.setMethodCallHandler(FlutterWebviewPlugin(registrar.activity()))
+        }
     }
-  }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        val intent = Intent(activity, WebViewActivity::class.java)
+        intent.putExtra(WebViewActivity.URL, call.argument<String>("url"))
+        activity.startActivity(intent)
+        activity.finish()
+    }
 }
--- android/src/main/kotlin/me/gr/flutter_webview/WebViewActivity.kt
+++ android/src/main/kotlin/me/gr/flutter_webview/WebViewActivity.kt
@@ -1,4 +1,65 @@
 package me.gr.flutter_webview
 
-class WebViewActivity {
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebViewActivity : Activity() {
+    private lateinit var webView: WebView
+
+    companion object {
+        const val URL = "url"
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            }
+        }
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl(intent.getStringExtra(URL))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.onResume()
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
+    }
 }
